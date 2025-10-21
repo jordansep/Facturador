3b9f855058d1492c9b114d6ab9f033ac40b73ecd
jordansep <jordanmonier4@gmail.com>
2025-10-21 16:04:50 -0300

    Agregar archivos de proyecto.

---
+++ Models/Tablas/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Facturador.Models.Clases
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id {get; private set; }
+        [Required]
+        public string CuitCuil { get; private set; }
+        [Required]
+        public string RazonSocial { get; private set; }
+        public string Domicilio { get; private set; }
+
+
+
+    }
+}
